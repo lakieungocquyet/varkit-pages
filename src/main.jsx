6ee4ed7163ec44b622735cbbd716d38f1846a4ff
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2025-11-24 22:20:14 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router'
+import { HashRouter  } from 'react-router'
 import App from './views/App.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   	<React.StrictMode>
-      	<BrowserRouter basename={"varkit-pages"}>
+      	<HashRouter  basename={"varkit-pages"}>
         	<App/>
-      	</BrowserRouter>
+      	</HashRouter >
   	</React.StrictMode>,
 )
